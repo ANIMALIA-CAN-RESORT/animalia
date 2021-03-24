33798992e03d900a10136a5fc440df7b2604fa34
Puentes <joseluispuentes@hotmail.com>
2021-03-24 11:30:06 +0100

    subir el proyecto recien creado de spring

---
+++ src/test/java/es/mde/residenciaDeAnimales/ResidenciaDeAnimalesApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mde.residenciaDeAnimales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidenciaDeAnimalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
