5e7955ce3130f3841bf944fa6447e482a406785f
joseluispuentes82 <67543258+joseluispuentes82@users.noreply.github.com>
2021-04-22 23:16:25 +0200

    primer commit con libreria enlazada

---
+++ src/main/java/es/lanyu/residenciaAnimalesapi/ResidenciaAnimalesapiApplication.java
@@ -0,0 +1,14 @@
+package es.lanyu.residenciaAnimalesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class ResidenciaAnimalesapiApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context =
+				SpringApplication.run(ResidenciaAnimalesapiApplication.class, args);
+	
+	}
+
+}
---
+++ src/test/java/es/lanyu/residenciaAnimalesapi/ResidenciaAnimalesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.lanyu.residenciaAnimalesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidenciaAnimalesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
