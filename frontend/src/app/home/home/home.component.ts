cf0115176bac17845eecd1508f955c9552f46808
Jose <joseluispuentes@hotmail.com>
2021-05-24 16:39:03 +0200

    Boceto coña home

--- frontend/src/app/home/home/home.component.ts
+++ frontend/src/app/home/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { faDog } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: []
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  faDog = faDog;
   constructor() { }
 
   ngOnInit() {
