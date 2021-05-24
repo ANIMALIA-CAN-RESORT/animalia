fc804c1730cae23aafd9678e2f96aaa66d66a68b
Jose <joseluispuentes@hotmail.com>
2021-05-24 16:50:13 +0200

    CRUD Alimentacion Fix #3

--- src/main/java/es/puentes/repositorios/AlimentacionDAO.java
+++ src/main/java/es/puentes/repositorios/AlimentacionDAO.java
@@ -1,13 +1,10 @@
 package es.puentes.repositorios;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import es.puentes.entidades.AlimentacionConId;
-import es.puentes.entidades.PrestacionConId;
 
 @RepositoryRestResource(path="alimentaciones", collectionResourceRel="alimentaciones", itemResourceRel="alimentacion")
 public interface AlimentacionDAO extends JpaRepository<AlimentacionConId, Long> {
