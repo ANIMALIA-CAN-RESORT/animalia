b51107e229e261f84ba24ef8202e1605509e644b
Jose <joseluispuentes@hotmail.com>
2021-05-24 16:49:58 +0200

    CRUD Alojamiento Fix #4

--- src/main/java/es/puentes/repositorios/AlojamientoDAO.java
+++ src/main/java/es/puentes/repositorios/AlojamientoDAO.java
@@ -1,10 +1,8 @@
 package es.puentes.repositorios;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import es.puentes.entidades.AlojamientoConId;
 
