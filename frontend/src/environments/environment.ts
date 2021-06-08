d6b0dd863f90b9b3d71cf851bbdfd74e3d57825a
Jose <joseluispuentes@hotmail.com>
2021-06-08 09:50:40 +0200

    aaaa

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   appName: 'Animalia DEV',
   production: false,
-  hostAnimalia: 'http://localhost:8080/api/',
-  // hostAnimalia: 'https://animalia-residencia-animales.herokuapp.com/api/',
+  // hostAnimalia: 'http://localhost:8080/api/',
+  hostAnimalia: 'https://animalia-residencia-animales.herokuapp.com/api/',
 };
 
 /*
