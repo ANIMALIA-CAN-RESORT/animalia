79a2106aadf5be0d573385385f95bd26c132a415
Puentes <joseluispuentes@hotmail.com>
2021-07-21 12:26:38 +0200

    github pages

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  appName: 'Angular Board PROD',
-  production: true,
-  hostAnimalia: 'https://animalia-dim43.herokuapp.com/api/'
-
-};
--- src/main/java/es/puentes/servicios/MailService.java
+++
@@ -1,8 +0,0 @@
-package es.puentes.servicios;
-
-import javax.mail.MessagingException;
-
-public interface MailService {
-
-	 String sendArchivo(String to, String subject, String body, String ruta) throws MessagingException;
-}
