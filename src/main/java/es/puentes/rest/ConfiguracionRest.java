27307a69bab5bf1786675626e4c1c96fb2928cde
joseluispuentes82 <joseluispuentes@hotmail.com>
2024-03-31 22:52:17 +0200

    Retocando los modales

--- src/main/java/es/puentes/rest/ConfiguracionRest.java
+++ src/main/java/es/puentes/rest/ConfiguracionRest.java
@@ -75,8 +75,8 @@ public class ConfiguracionRest {
 	CorsFilter corsFilter() {
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		final CorsConfiguration config = new CorsConfiguration();
-		//config.setAllowCredentials(true);
-		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowCredentials(true);
+		config.setAllowedOriginPatterns(Collections.singletonList("*"));
 		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
 		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
 		source.registerCorsConfiguration("/**", config);
